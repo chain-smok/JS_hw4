9d794c17b7f3456a224fd8c42374fe9448ba7ab6
chain-smok <s1101780@gm.pu.edu.tw>
2022-12-10 22:19:13 +0800

    hw4 finish

---
+++ hw4/weixun.js
@@ -0,0 +1,8 @@
+
+      function welcome(){
+        str1="please enter your name?";
+        var name=prompt(str1);
+        window.alert("HELLO "+name);
+        node=document.getElementById("p4");
+        node.innerHTML=" Come here "+name;   
+      }
